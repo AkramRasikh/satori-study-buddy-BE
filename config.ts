2bf478a1c3b38cd914bdfa1a7aa51f74edeb1547
Akram Rasikh <akram.rasikh@gmail.com>
2024-05-08 17:06:52 +1000

    firebase db config setup

--- config.ts
+++ config.ts
@@ -4,6 +4,7 @@ dotenv.config();
 const config = {
   port: process.env.PORT || 3001,
   firebaseBucketName: process.env.FIREBASE_BUCKET_NAME,
+  firebaseDBUrl: process.env.FIREBASE_DB_URL,
 };
 
 export default config;
