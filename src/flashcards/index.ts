8c0b0697d42fa0930e8b8d559dab5a3bfae458f5
Akram Rasikh <akram.rasikh@gmail.com>
2024-07-10 16:57:29 +0100

    moved function and getting words via topic key

--- src/flashcards/index.ts
+++ src/flashcards/index.ts
@@ -18,4 +18,136 @@ const getJapaneseSongs = async () => {
   return data;
 };
 
-export { getJapaneseWords, getJapaneseContent, getJapaneseSongs };
+const getJapaneseWordsViaTopic = async ({ topic }) => {
+  const japaneseWordsRes = await getJapaneseWords();
+  const japaneseContentRes = await getJapaneseContent();
+
+  const contextIds = [];
+  const contextData = [];
+
+  japaneseWordsRes.forEach((word) => {
+    const contexts = word.contexts;
+    contexts.forEach((singleContext) => {
+      if (!contextIds.includes(singleContext)) {
+        contextIds.push(singleContext);
+      }
+    });
+  });
+
+  const prefixName = topic.split('-')[0];
+  console.log('####### prefixName', prefixName);
+
+  const topicKeys = Object.keys(japaneseContentRes).filter(
+    (thisTopic) => thisTopic.split('-')[0] === prefixName,
+  );
+
+  topicKeys.forEach((thisTopicKey) => {
+    const thisTopicArr = japaneseContentRes[thisTopicKey];
+    thisTopicArr.forEach((sentenceData) => {
+      if (contextIds.includes(sentenceData.id)) {
+        const contextDataObj = {
+          topic: thisTopicKey,
+          ...sentenceData,
+        };
+        contextData.push(contextDataObj);
+      }
+    });
+  });
+  const lastMap = [];
+  japaneseWordsRes.forEach((word) => {
+    const contexts = word.contexts;
+
+    const contextToData = contextData.filter((contextDataWidget) =>
+      contexts.includes(contextDataWidget.id),
+    );
+    if (contextToData?.length > 0) {
+      lastMap.push({
+        ...word,
+        contextToData,
+      });
+    }
+  });
+
+  return lastMap;
+};
+
+const getCollectionOfWordData = async () => {
+  const japaneseWordsRes = await getJapaneseWords();
+  const japaneseContentRes = await getJapaneseContent();
+  const japaneseSongsRes = await getJapaneseSongs();
+  const japaneseContentTopicKeys = Object.keys(japaneseContentRes);
+
+  const contextIds = [];
+  const contextData = [];
+  const topicsForAudio = [];
+
+  japaneseWordsRes.forEach((word) => {
+    const contexts = word.contexts;
+    contexts.forEach((singleContext) => {
+      if (!contextIds.includes(singleContext)) {
+        contextIds.push(singleContext);
+      }
+    });
+  });
+
+  japaneseContentTopicKeys.forEach((topic) => {
+    const thisTopicArr = japaneseContentRes[topic];
+    thisTopicArr.forEach((sentenceData) => {
+      if (contextIds.includes(sentenceData.id)) {
+        const contextDataObj = {
+          topic,
+          ...sentenceData,
+        };
+        contextData.push(contextDataObj);
+        if (!topicsForAudio.includes(topic)) {
+          topicsForAudio.push(topic);
+        }
+      }
+    });
+  });
+
+  // until no more contextIds
+  japaneseSongsRes.forEach((song) => {
+    const lyrics = song.lyrics;
+    const title = song.title;
+    lyrics.forEach((sentenceData) => {
+      if (contextIds.includes(sentenceData.id)) {
+        const contextDataObj = {
+          topic: title,
+          isMusic: true,
+          ...sentenceData,
+        };
+        contextData.push(contextDataObj);
+        if (!topicsForAudio.includes(title)) {
+          topicsForAudio.push(title);
+        }
+      }
+    });
+  });
+
+  const wordToContextData = japaneseWordsRes.map((word) => {
+    const contexts = word.contexts;
+
+    const contextToData = contextData.filter((contextDataWidget) =>
+      contexts.includes(contextDataWidget.id),
+    );
+
+    return {
+      ...word,
+      contextToData,
+    };
+  });
+  return {
+    wordToContextData,
+    topics: topicsForAudio,
+    flashCardData: contextData,
+  };
+};
+
+export {
+  getJapaneseWords,
+  getJapaneseContent,
+  getJapaneseSongs,
+  getCollectionOfWordData,
+  getJapaneseWordsViaTopic,
+};
--- src/flashcards/routes.ts
+++ src/flashcards/routes.ts
@@ -1,82 +1,21 @@
 import { Request, Response } from 'express';
-import { getJapaneseContent, getJapaneseSongs, getJapaneseWords } from '.';
+import { getCollectionOfWordData, getJapaneseWordsViaTopic } from '.';
 
 const flashcardRoutes = async (app) => {
   app.get('/get-japanese-words', async (req: Request, res: Response) => {
     try {
-      const japaneseWordsRes = await getJapaneseWords();
-      const japaneseContentRes = await getJapaneseContent();
-      const japaneseSongsRes = await getJapaneseSongs();
-      const japaneseContentTopicKeys = Object.keys(japaneseContentRes);
+      const collectionOfWordData = await getCollectionOfWordData();
+      res.send(collectionOfWordData).status(200);
+    } catch (error) {
+      console.log('## error get-japanese-words', error);
+    }
+  });
 
-      const contextIds = [];
-      const contextData = [];
-      const topicsForAudio = [];
-
-      japaneseWordsRes.forEach((word) => {
-        const contexts = word.contexts;
-        contexts.forEach((singleContext) => {
-          if (!contextIds.includes(singleContext)) {
-            contextIds.push(singleContext);
-          }
-        });
-      });
-
-      japaneseContentTopicKeys.forEach((topic) => {
-        const thisTopicArr = japaneseContentRes[topic];
-        thisTopicArr.forEach((sentenceData) => {
-          if (contextIds.includes(sentenceData.id)) {
-            const contextDataObj = {
-              topic,
-              ...sentenceData,
-            };
-            contextData.push(contextDataObj);
-            if (!topicsForAudio.includes(topic)) {
-              topicsForAudio.push(topic);
-            }
-          }
-        });
-      });
-
-      // until no more contextIds
-      japaneseSongsRes.forEach((song) => {
-        const lyrics = song.lyrics;
-        const title = song.title;
-        lyrics.forEach((sentenceData) => {
-          if (contextIds.includes(sentenceData.id)) {
-            const contextDataObj = {
-              topic: title,
-              isMusic: true,
-              ...sentenceData,
-            };
-            contextData.push(contextDataObj);
-            if (!topicsForAudio.includes(title)) {
-              topicsForAudio.push(title);
-            }
-          }
-        });
-      });
-
-      const wordToContextData = japaneseWordsRes.map((word) => {
-        const contexts = word.contexts;
-
-        const contextToData = contextData.filter((contextDataWidget) =>
-          contexts.includes(contextDataWidget.id),
-        );
-
-        return {
-          ...word,
-          contextToData,
-        };
-      });
-
-      res
-        .send({
-          wordToContextData,
-          // topics: topicsForAudio,
-          // flashCardData: contextData,
-        })
-        .status(200);
+  app.post('/get-japanese-words-topic', async (req: Request, res: Response) => {
+    const topic = req.body?.topic;
+    try {
+      const thisTopicsWords = await getJapaneseWordsViaTopic({ topic });
+      res.send(thisTopicsWords).status(200);
     } catch (error) {
       console.log('## error get-japanese-words', error);
     }
