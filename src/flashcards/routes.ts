2b892a7406fc69f20df96ef1548439d6da4c3b68
Akram Rasikh <akram.rasikh@gmail.com>
2024-07-10 12:53:59 +0100

    getting base for langauges

--- src/flashcards/routes.ts
+++ src/flashcards/routes.ts
@@ -1,15 +1,53 @@
 import { Request, Response } from 'express';
-// import { getJapaneseContent, getJapaneseSongs, getJapaneseWords } from '.';
-import { getJapaneseWords } from '.';
+import { getJapaneseContent, getJapaneseSongs, getJapaneseWords } from '.';
 
 const flashcardRoutes = async (app) => {
   app.get('/get-japanese-words', async (req: Request, res: Response) => {
     try {
       const japaneseWordsRes = await getJapaneseWords();
-      // const japaneseContentRes = await getJapaneseContent();
-      // const japaneseSongsRes = await getJapaneseSongs()
+      const japaneseContentRes = await getJapaneseContent();
+      const japaneseSongsRes = await getJapaneseSongs();
+      const japaneseContentTopicKeys = Object.keys(japaneseContentRes);
+      const contextIds = [];
+      japaneseWordsRes.forEach((word) => {
+        const contexts = word.contexts;
+        contexts.forEach((singleContext) => {
+          if (!contextIds.includes(singleContext)) {
+            contextIds.push(singleContext);
+          }
+        });
+      });
 
-      res.send(japaneseWordsRes).status(200);
+      const contextData = [];
+      japaneseContentTopicKeys.forEach((topic) => {
+        const thisTopicArr = japaneseContentRes[topic];
+        thisTopicArr.forEach((sentenceData) => {
+          if (contextIds.includes(sentenceData.id)) {
+            const contextDataObj = {
+              topic,
+              ...sentenceData,
+            };
+            contextData.push(contextDataObj);
+          }
+        });
+      });
+
+      // until no more contextIds
+      japaneseSongsRes.forEach((song) => {
+        const lyrics = song.lyrics;
+        const title = song.title;
+        lyrics.forEach((sentenceData) => {
+          if (contextIds.includes(sentenceData.id)) {
+            const contextDataObj = {
+              topic: title,
+              ...sentenceData,
+            };
+            contextData.push(contextDataObj);
+          }
+        });
+      });
+
+      res.send(contextData).status(200);
     } catch (error) {
       console.log('## error get-japanese-words', error);
     }
