b2253c2e4cab1f436c9753af0514c1e81ce208c8
Akram Rasikh <akram.rasikh@gmail.com>
2024-10-16 14:29:40 +0100

    content types established

--- src/firebase/refs.ts
+++ src/firebase/refs.ts
@@ -4,3 +4,12 @@ export const words = 'words';
 export const sentences = 'sentences';
 export const adhocSentences = 'adhocSentences';
 export const snippets = 'snippets';
+
+export enum ContentTypes {
+  Content = 'content',
+  Songs = 'songs',
+  Words = 'words',
+  Sentences = 'sentences',
+  AdhocSentences = 'adhocSentences',
+  Snippets = 'snippets',
+}
