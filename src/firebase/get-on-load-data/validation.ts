646521d349908a2c12a030ceee197e87e50e019c
Akram Rasikh <akram.rasikh@gmail.com>
2024-10-18 16:14:36 +0100

    updated oword update

--- src/firebase/get-on-load-data/validation.ts
+++ src/firebase/get-on-load-data/validation.ts
@@ -3,23 +3,25 @@ import { languageValidation } from '../../route-validation/check-mandatory-langu
 import { body } from 'express-validator';
 import { eligibleRefs } from '../refs';
 
+const checkRefsEligibility = (refs: string[]) => {
+  const refsAreEligible = refs.every((ref: string) =>
+    eligibleRefs.includes(ref),
+  );
+  if (refsAreEligible) {
+    return true;
+  }
+  throw new Error(
+    `Wrong refs passed. Available refs: ${eligibleRefs.join(', ')}`,
+  );
+};
+
 const getOnLoadDataValidation = [
   ...languageValidation,
   body('refs')
     .notEmpty()
     .isArray()
     .withMessage(`Refs are required: ${eligibleRefs.join(', ')}`)
-    .custom((refs) => {
-      const refsAreEligible = refs.every((ref: string) =>
-        eligibleRefs.includes(ref),
-      );
-      if (refsAreEligible) {
-        return true;
-      }
-      throw new Error(
-        `Wrong refs passed. Available refs: ${eligibleRefs.join(', ')}`,
-      );
-    }),
+    .custom(checkRefsEligibility),
 ];
 
 export { getOnLoadDataValidation };
--- src/firebase/update-word/validation.ts
+++ src/firebase/update-word/validation.ts
@@ -5,33 +5,35 @@ import {
 } from '../body-validation-types';
 import { languageValidation } from '../../route-validation/check-mandatory-language';
 
+const updateFieldForWordValidation = (value: object) => {
+  if (!value || typeof value !== 'object') {
+    throw new Error(
+      `fieldToUpdate must be an object of: 
+      ${wordKeysRouteValidationArr.join(', ')}`,
+    );
+  }
+
+  const hasValidField = Object.keys(value).some((key) =>
+    wordKeysRouteValidationArr.includes(key),
+  );
+  if (!hasValidField) {
+    throw new Error(
+      `fieldToUpdate must contain at least one of the following: ${wordKeysRouteValidationArr.join(
+        ', ',
+      )}`,
+    );
+  }
+
+  return true;
+};
+
 const updateWordValidation = [
   ...languageValidation,
   body(updateWordObj.wordId)
     .notEmpty()
     .isString()
     .withMessage('wordId is required for an update'),
-  body(updateWordObj.fieldToUpdate).custom((value) => {
-    if (!value || typeof value !== 'object') {
-      throw new Error(
-        `fieldToUpdate must be an object of: 
-        ${wordKeysRouteValidationArr.join(', ')}`,
-      );
-    }
-
-    const hasValidField = Object.keys(value).some((key) =>
-      wordKeysRouteValidationArr.includes(key),
-    );
-    if (!hasValidField) {
-      throw new Error(
-        `fieldToUpdate must contain at least one of the following: ${wordKeysRouteValidationArr.join(
-          ', ',
-        )}`,
-      );
-    }
-
-    return true;
-  }),
+  body(updateWordObj.fieldToUpdate).custom(updateFieldForWordValidation),
 ];
 
 export { updateWordValidation };
