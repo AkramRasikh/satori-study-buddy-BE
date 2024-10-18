95e00afb1a0dfa022872a55a66fde3898d8b9b1a
Akram Rasikh <akram.rasikh@gmail.com>
2024-10-18 18:29:43 +0100

    added validation function for check

--- src/firebase/delete-snippet/route.ts
+++ src/firebase/delete-snippet/route.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from 'express';
 import { deleteSnippetLogic } from './delete-snippet-logic';
+import { validationResult } from 'express-validator';
 
 const deleteSnippet = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   const id = req.body.id;
   const language = req.body.language;
   try {
