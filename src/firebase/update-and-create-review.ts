be957b2aa430981039d7dc2164e8effec817f268
Akram Rasikh <akram.rasikh@gmail.com>
2024-10-23 11:21:35 +0100

    types added

--- src/firebase/update-and-create-review.ts
+++ src/firebase/update-and-create-review.ts
@@ -1,11 +1,13 @@
+import { ContentType, SentenceType } from './types';
+
 export const getThisContentsIndex = ({ data, title }) => {
   // Convert object of objects to an array
   const values = Object.values(data);
   const keys = Object.keys(data);
 
   // Find the index of the object to update
-  const index = values.findIndex((item) => {
-    return (item as any).title === title;
+  const index = values.findIndex((item: ContentType) => {
+    return item.title === title;
   });
 
   return { keys, index };
@@ -17,8 +19,8 @@ export const getThisSentenceIndex = ({ data, id }) => {
   const sentenceKeys = Object.keys(data);
 
   // Find the index of the object to update
-  const sentenceIndex = values.findIndex((item) => {
-    return (item as any).id === id;
+  const sentenceIndex = values.findIndex((item: SentenceType) => {
+    return item.id === id;
   });
 
   return { sentenceKeys, sentenceIndex };
