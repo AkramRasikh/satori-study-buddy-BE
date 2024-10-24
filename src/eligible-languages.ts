27633cc2c0c5b0561d9e95dde18ecb14f2ac62ea
Akram Rasikh <akram.rasikh@gmail.com>
2024-10-24 08:47:44 +0100

    updated google translate

--- src/eligible-languages.ts
+++ src/eligible-languages.ts
@@ -2,3 +2,7 @@ const japanese = 'japanese';
 const chinese = 'chinese';
 
 export const eligibleLanguages = [japanese, chinese];
+export const googleLanguagesKey = {
+  [japanese]: 'ja',
+  [chinese]: 'zh-CN',
+};
--- src/language-script-helpers/google-translate.ts
+++ src/language-script-helpers/google-translate.ts
@@ -1,20 +1,31 @@
 import { translate } from '@vitalets/google-translate-api';
+import { googleLanguagesKey } from '../eligible-languages';
 
-const getGoogleTranslate = async (word) => {
-  const { text: definition, raw } = (await translate(word, {
-    from: 'ja',
-    to: 'en',
-  })) as any;
+const getGoogleTranslate = async ({ word, language }) => {
+  const fromLanguage = googleLanguagesKey[language];
 
-  let transliteration: string[] = [];
-  raw.sentences?.forEach((sentence) => {
-    if (sentence?.src_translit) {
-      transliteration.push(sentence.src_translit);
+  try {
+    if (fromLanguage) {
+      const { text: definition, raw } = (await translate(word, {
+        from: fromLanguage,
+        to: 'en',
+      })) as any;
+
+      let transliteration: string[] = [];
+      raw.sentences?.forEach((sentence) => {
+        if (sentence?.src_translit) {
+          transliteration.push(sentence.src_translit);
+        }
+      });
+      const finalTransliteration = transliteration?.join(' ');
+
+      return { definition, transliteration: finalTransliteration };
+    } else {
+      throw new Error(`No viable language key found for ${language}`);
     }
-  });
-  const finalTransliteration = transliteration?.join(' ');
-
-  return { definition, transliteration: finalTransliteration };
+  } catch (error) {
+    throw new Error(error || 'Error getting google translation');
+  }
 };
 
 export { getGoogleTranslate };
