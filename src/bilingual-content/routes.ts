732f0d18af83672c66c5edbf3ac697cde67243f7
Akram Rasikh <akram.rasikh@gmail.com>
2025-02-12 21:20:48 +0000

    youtube-to-audio-snippets removed

--- src/bilingual-content/routes.ts
+++ src/bilingual-content/routes.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import fs from 'fs';
 import { Request, Response } from 'express';
-import { content } from '../firebase/refs';
 import { uploadBufferToFirebase } from '../firebase/init';
 import { extractYoutubeAudio } from './extract-youtube-audio';
 import {
@@ -10,8 +9,6 @@ import {
   splitByInterval,
   splitSubtitlesByInterval,
 } from './output/youtube-txt-file';
-import { getRefPath } from '../utils/get-ref-path';
-import { checkMandatoryLanguage } from '../route-validation/check-mandatory-language';
 import { addContentLogic } from '../firebase/add-content/add-content-logic';
 import { getAudioFolderViaLang } from '../utils/get-audio-folder-via-language';
 import { cutAudioFromAudio } from '../mp3-utils/cut-audio-from-audio';
@@ -27,63 +24,6 @@ export const outputFile = (title) => {
 
 const bilingualContentRoutes = (app) => {
   app.post('/get-subtitles', youtubeVideoToBilingualText);
-  app.post(
-    '/youtube-to-audio-snippets',
-    checkMandatoryLanguage,
-    async (req: Request, res: Response) => {
-      const url = req?.body?.url;
-      const title = req?.body?.title;
-      const splits = req?.body?.interval;
-      const language = req?.body?.language;
-      const refPath = getRefPath({ ref: content, language });
-
-      await extractYoutubeAudio({ url, title });
-      const filePath = path.resolve(__dirname, 'output', `${title}.txt`);
-      const mp3FileInput = path.resolve(__dirname, 'output', `${title}.mp3`);
-
-      const outputJson = splitSubtitlesByInterval(filePath, null, null, null);
-      const resFromChunking = splitByInterval(outputJson, splits, title);
-      const updateToAndFromValues = getUpdateToAndFromValues(resFromChunking);
-      try {
-        // Sequential processing using for...of and await
-        for (const item of updateToAndFromValues) {
-          const audioPath = outputFile(item.title);
-          const extractedSectionIndex = await extractMP3Section(
-            mp3FileInput,
-            outputFile(item.title),
-            item.from,
-            item.to,
-          );
-
-          const fileBuffer = fs.readFileSync(audioPath);
-          const formattedFirebaseName = 'japanese-audio/' + item.title + '.mp3';
-
-          // Upload audio snippet to Firebase
-          await uploadBufferToFirebase({
-            buffer: fileBuffer,
-            filePath: formattedFirebaseName,
-          });
-
-          // Add content metadata to Firebase
-          await addContentLogic({
-            language,
-            content: {
-              title: item.title,
-              hasAudio: item.hasAudio,
-              origin: youtube,
-              content: item.content,
-              url,
-              interval: splits,
-              realStartTime: item.from,
-            },
-          });
-        }
-      } catch (error) {
-        console.error('Error creating snippets:', error);
-      }
-    },
-  );
-
   app.post(
     '/youtube-to-audio-snippets-nested',
     async (req: Request, res: Response) => {
