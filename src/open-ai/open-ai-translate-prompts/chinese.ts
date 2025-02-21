a6f0e07b8bd26dd181d0ca3f1336ef6c2d5d4cb9
Akram Rasikh <akram.rasikh@gmail.com>
2025-02-21 14:50:30 +0000

    chinese ai definition working

--- src/open-ai/open-ai-translate-prompts/chinese.ts
+++ src/open-ai/open-ai-translate-prompts/chinese.ts
@@ -9,26 +9,25 @@ const wordProperties = {
 };
 
 const jsonFormatBasicExample = {
-  definition: 'beans, bean, peas',
-  transliteration: 'Mame',
-  transliterationSurfaceForm: 'Mame',
-  note: '豆',
-  baseForm: '豆',
-  phonetic: 'まめ',
-  surfaceForm: '豆',
+  baseForm: '扒手',
+  surfaceForm: '扒手',
+  definition: 'pickpocket',
+  transliteration: 'Páshǒu',
+  transliterationSurfaceForm: 'Páshǒu',
+  phonetic: 'Páshǒu',
   notes:
-    'beans here is flexible ranging from coffee beans to beans cooked for food such as lentils/fava beans, etc',
+    "扒手 (pāshǒu) means pickpocket in Chinese. It refers to someone who steals small items, usually money or valuables, from others' pockets or bags without them noticing",
 };
 
 const jsonFormatVerbExample = {
-  baseForm: '彷徨う',
-  surfaceForm: '彷徨って',
-  definition: 'to wander or to roam',
-  transliteration: 'samayou',
-  transliterationSurfaceForm: 'samayotte',
-  phonetic: 'さまよう',
+  definition: 'Two Less, One Leniency policy',
+  transliteration: 'Liǎng shǎo yī kuān',
+  transliterationSurfaceForm: 'Liǎng shǎo yī kuān',
+  baseForm: '两少一宽',
+  phonetic: 'Liǎng shǎo yī kuān',
+  surfaceForm: '两少一宽',
   notes:
-    'By itself, 彷徨 (ほうこう, hōkō) means "wandering" or "roaming." It describes the state of wandering aimlessly, much like the verb 彷徨う (samayou), but in its noun form.',
+    'The "Two Less, One Leniency" (两少一宽) policy in 1980s China aimed to reduce arrests and death penalties for ethnic minorities and juvenile offenders, while granting them more lenient sentencing. It sought to promote social stability and rehabilitation over harsh punishment',
 };
 
 const baseFormPrompt = `${wordProperties.baseForm}: Give me the base form of the word provided. For example the base form (infinite form) of 泳げます is 泳ぐ. If possible, also do this when multiple verbs are used together for example 勉強して遊びたい => 勉強して遊ぶ. However, if this detracts from the meaning then leave it as the surface level provided i.e. 勉強して遊びたい. If it is a phrase then leave it as it is for example じゃあいってきます can be left as it is.`;
@@ -37,10 +36,10 @@ const transliterationPrompt = `${wordProperties.transliteration}: is the transli
 const transliterationSurfaceFormPrompt = `${wordProperties.transliterationSurfaceForm}: is the transliteration of the ${wordProperties.surfaceForm}.`;
 const phoneticPrompt = `${wordProperties.phonetic}: is the romanji/katakana of the ${wordProperties.baseForm}.`;
 
-const chineseformatTranslationPrompt = (japaneseWord, context) => {
+const chineseformatTranslationPrompt = (chineseWord, context) => {
   return `
   
-  Translate the below word from Japanese to english given the context.
+  Translate the below word from Chinese to english given the context.
   I want the definition, transliteration, transliterationSurfaceForm, phonetic, baseForm and notes section.
 
   ${JSON.stringify(baseFormPrompt)}
@@ -49,9 +48,10 @@ const chineseformatTranslationPrompt = (japaneseWord, context) => {
   ${JSON.stringify(transliterationSurfaceFormPrompt)}
   ${JSON.stringify(phoneticPrompt)}
 
-  For example, given 豆 I want the return object:
-
-  ${JSON.stringify(jsonFormatBasicExample)}
+  For example, given ${
+    jsonFormatBasicExample.surfaceForm
+  } I want the return object:
+    ${JSON.stringify(jsonFormatBasicExample)}
 
   A more comprehensive example for ${
     jsonFormatVerbExample.surfaceForm
@@ -61,7 +61,7 @@ const chineseformatTranslationPrompt = (japaneseWord, context) => {
 
   NOTE: this is an integration so only the above is indeed as a response.
 
-  Word to translate: ${japaneseWord}
+  Word to translate: ${chineseWord}
   Context in which the word is used ${context}
 `;
 };
