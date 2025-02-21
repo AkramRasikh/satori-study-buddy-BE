cdb8f963164c36aba22d575509dd3d6afb5bca76
Akram Rasikh <akram.rasikh@gmail.com>
2025-02-21 22:27:02 +0000

    removed bilingual code

--- src/bilingual-content/time-to-seconds.ts
+++
@@ -1,6 +0,0 @@
-const timeToSeconds = (time) => {
-  const [hours, minutes, seconds] = time.split(':').map(Number);
-  return hours * 3600 + minutes * 60 + seconds;
-};
-
-export { timeToSeconds };
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import { firebaseRoutes } from './firebase/routes';
 import { languageScriptHelpers } from './language-script-helpers/routes';
 import { narakeetRoutes } from './narakeet/routes';
 import { mp3Utils } from './mp3-utils/routes';
-import { bilingualContentRoutes } from './bilingual-content/routes';
 
 const app = express();
 
@@ -22,7 +21,6 @@ firebaseRoutes(app);
 languageScriptHelpers(app);
 narakeetRoutes(app);
 mp3Utils(app);
-bilingualContentRoutes(app);
 
 app.listen(port, () => {
   console.log(`## Server is running at http://localhost:${port}`);
